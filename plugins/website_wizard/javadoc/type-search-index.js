2e558f57c9d303a0caefb5d00f6d019c7c41c3c2
Julian Tigler <High5Apps@gmail.com>
2026-08-25 23:24:53 -0700

    Add autocreated javadoc

---
+++ plugins/website_wizard/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.openstreetmap.josm.plugins.websitewizard","l":"WebsiteWizardDialog"},{"p":"org.openstreetmap.josm.plugins.websitewizard","l":"WebsiteWizardPlugin"}];updateSearchResults();
